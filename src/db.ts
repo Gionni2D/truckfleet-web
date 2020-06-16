36209c9f72a96afc2b3c24bea1fff53f304e8d8d
Gionni2D <gionni2d@gmail.com>
2020-06-16 11:19:49 +0200

    Initialize fake db

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+import { Magazzino, Ordine, Tappa, Spedizione } from "./domain";
+
+export const Spedizioni : Spedizione[] = [
+
+]
+export const Ordini : Ordine[] = [
+
+]
+export const Magazzini : Magazzino[] = [
+
+]
+export const Tappe : Tappa[] = [
+
+]
