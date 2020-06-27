732638f27e39ceb716d9f5744ed925be76ae5c4a
Gionni2D <gionni2d@gmail.com>
2020-06-27 12:55:45 +0200

    Graphics of GestioneOrdineView

--- src/routes/GestioneOrdini/GestioneOrdiniView.tsx
+++ src/routes/GestioneOrdini/GestioneOrdiniView.tsx
@@ -2,9 +2,14 @@ import { i18n } from '../../i18n'
 import { RouteList } from '..'
 import * as React from 'react'
 import app from '../../app'
-import { Ordine } from "../../domain"
+import { Ordine, StatoOrdine } from "../../domain"
 import Drawer from '../../components/Drawer'
 import App from '../../components/App'
+import { Grid, Typography, IconButton, Avatar, Fab, TextField } from '@material-ui/core'
+import { Card, CardContent, CardHeader } from '@material-ui/core'
+import OrderIcon from '@material-ui/icons/ListAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
 
 interface ViewProps {
 	ordini : Ordine[]
@@ -12,6 +17,24 @@ interface ViewProps {
 	onChangeFilterText(filterText: string): void
 	onEliminaOrdine(idOrdine: number): boolean
 	filterText: string
+}
+
+const style : { [key: string] : React.CSSProperties } = {
+	fab: {
+		position: 'fixed',
+		bottom: "20px",
+		right: "20px"
+	},
+	card: {
+		padding: 10,
+		margin: "20px 0"
+	},
+	mTitleContent: {
+		marginTop: 20
+	},
+	mtText: {
+		marginTop: 10
+	}
 }
 
 export type FromGestioneOrdiniRoute =
@@ -36,7 +59,7 @@ export default class GestioneOrdiniView
 		this.props.onChangeFilterText(e.target.value);
 	}
 
-	onEliminaOrdine = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, idOrdine: number) => {
+	onEliminaOrdine = (idOrdine: number) => {
 		if(this.props.onEliminaOrdine(idOrdine))
 			alert("Ordine eliminato correttamente");
 		else
@@ -44,49 +67,78 @@ export default class GestioneOrdiniView
 	}
 
 	render() {
-		const b = this.bundle.routes.gestioneOrdini
+		const b  = this.bundle.routes.gestioneOrdini
+		const bd = this.bundle.domain
 
-	//starebbe bene anche degli spazi tra i due bottoni piuttosto che andare a capo
-	return <App>
-	<Drawer >
-		<h1>{b.manageOrders}</h1><div>
-		<button onClick={this.onChangeRoute.bind(this, RouteList.Home)}>Home</button><br/><br/>
-		<input type="text" onChange={this.onChangeFilterText} value={this.props.filterText} placeholder={`${b.search}...`}></input>
-		<button onClick={this.onChangeRoute.bind(this, RouteList.InserisciOrdine)}>{b.insertOrder}</button>
-		<table>
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>{b.ordersProperty.desc}</th>
-					<th>{b.ordersProperty.from}</th>
-					<th>{b.ordersProperty.to}</th>
-					<th>{b.ordersProperty.load}</th>
-					<th>{b.ordersProperty.unload}</th>
-					<th>{b.ordersProperty.dimension}</th>
-					<th>{b.ordersProperty.mass}</th>
-					<th>{b.ordersProperty.state}</th>
-					<th>{b.actions}</th>
-				</tr>
-			</thead>
-			<tbody>
-				{ this.props.ordini.map((ordine) => {
-					return (<tr key={ordine.id}>
-						<th>{ordine.id}</th>
-						<td>{ordine.descrizione}</td>
-						<td>{ordine.nomeMittente}</td>
-						<td>{ordine.nomeDestinatario}</td>
-						<td>{ordine.getInfoCarico()[0].indirizzo}</td>
-						<td>{ordine.getInfoScarico()[0].indirizzo}</td>
-						<td>{ordine.dimX}x{ordine.dimY}x{ordine.dimZ}cm</td>
-						<td>{ordine.massa}kg</td>
-						<td>{this.bundle.domain.orderState[ordine.stato]}</td>
-						<td><button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => this.onEliminaOrdine(e, ordine.id)}>{b.delete}</button></td>
-					</tr>)
-				})}
-			</tbody>
-		</table>
-		</div>
-		</Drawer>
+		return <App>
+			<Drawer >
+				<Typography variant="h3" >{b.manageOrders}</Typography>
+
+				<div style={style.mTitleContent}>
+					<TextField
+						label={`${b.search}...`}
+						value={this.props.filterText}
+						onChange={this.onChangeFilterText} />
+
+					<Fab color="primary" style={style.fab}>
+						<AddIcon />
+					</Fab>
+
+					{ this.props.ordini.map((ordine) =>
+						<Card key={ordine.id} style={style.card}>
+							<CardHeader
+								avatar={<Avatar><OrderIcon /></Avatar>}
+								title={ordine.descrizione}
+								subheader={<span>
+									{b.ordersProperty.id}: <b>{ordine.id}</b><br/>
+									{b.ordersProperty.state}: <b>{bd.orderState[ordine.stato]}</b>
+								</span>}
+								action={ ordine.stato == StatoOrdine.INSERITO ?
+									<IconButton
+										onClick={this.onEliminaOrdine.bind(this, ordine.id)}>
+										<DeleteIcon />
+									</IconButton>:
+									undefined
+								}
+								/>
+							<CardContent>
+								<Grid container spacing={2}>
+									<Grid item xs={4}>
+										<div>
+											<Typography variant="overline" color="textSecondary">{b.ordersProperty.from}:</Typography>
+											<Typography variant="body2">{ordine.nomeMittente}</Typography>
+										</div>
+										<div style={style.mt}>
+											<Typography variant="overline" color="textSecondary">{b.ordersProperty.to}:</Typography>
+											<Typography variant="body2">{ordine.nomeDestinatario}</Typography>
+										</div>
+									</Grid>
+									<Grid item xs={4}>
+										<div>
+											<Typography variant="overline" color="textSecondary">{b.ordersProperty.load}:</Typography>
+											<Typography variant="body2">{ordine.getInfoCarico()[0].indirizzo}</Typography>
+										</div>
+										<div style={style.mt}>
+											<Typography variant="overline" color="textSecondary">{b.ordersProperty.unload}:</Typography>
+											<Typography variant="body2">{ordine.getInfoScarico()[0].indirizzo}</Typography>
+										</div>
+									</Grid>
+									<Grid item xs={4}>
+										<div>
+											<Typography variant="overline" color="textSecondary">{b.ordersProperty.dimension}:</Typography>
+											<Typography variant="body2">{ordine.dimX}x{ordine.dimY}x{ordine.dimZ}cm</Typography>
+										</div>
+										<div style={style.mt}>
+											<Typography variant="overline" color="textSecondary">{b.ordersProperty.mass}:</Typography>
+											<Typography variant="body2">{ordine.massa}kg</Typography>
+										</div>
+									</Grid>
+								</Grid>
+							</CardContent>
+						</Card>
+					)}
+				</div>
+			</Drawer>
 		</App>
 	}
 }
