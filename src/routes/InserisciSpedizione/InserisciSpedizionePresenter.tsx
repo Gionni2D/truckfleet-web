6af3ff1985a618f3c407a554fd52e87d843739c8
Gionni2D <gionni2d@gmail.com>
2020-06-28 11:44:26 +0200

    Implement InserisciSpedizionePresenter

--- src/routes/InserisciSpedizione/InserisciSpedizionePresenter.tsx
+++ src/routes/InserisciSpedizione/InserisciSpedizionePresenter.tsx
@@ -1,8 +1,15 @@
 import InserisciSpedizioneView from './InserisciSpedizioneView'
 import * as React from 'react'
 import app from '../../app'
+import { SpedizioneRaw, Camionista, Ordine, StatoOrdine, TappaRaw } from '../../domain'
 
 interface PresenterState {
+	spedizione: SpedizioneRaw,
+	tappe: TappaRaw[],
+	camionisti: Camionista[],
+	dataInizio: number,
+	optimization: boolean,
+	ordini: Ordine[]
 }
 
 export default class InserisciSpedizionePresenter
@@ -10,11 +17,57 @@ export default class InserisciSpedizionePresenter
 
 	constructor(props: {}) {
 		super(props)
+
+		const camionisti = app.getCamionisti();
+		const ordini = app.getOrdini(o => o.stato == StatoOrdine.INSERITO);
+
 		this.state = {
+			camionisti,
+			optimization: false,
+			dataInizio: Date.now() + 1000*60*60*24,
+			ordini,
+			spedizione: {
+				veicoloTarga: "",
+				veicoloModello: "",
+				veicoloMassa: 0,
+				rimorchioDimX: 0,
+				rimorchioDimY: 0,
+				rimorchioDimZ: 0,
+				rimorchioCaricoMax: 0,
+				rimorchioMassa: 0,
+				camionisti: [camionisti[0]]
+			},
+			tappe: []
 		}
 	}
 
+	optimizeSpedizione() {
+		// TODO
+	}
+
+	onInfoVeicoliInserted = () => {
+		if(this.state.optimization) this.optimizeSpedizione()
+	}
+
+	onAllInfoInserted = () => {
+		const {spedizione, tappe, dataInizio} = this.state
+		return app.validaSpedizione(spedizione, tappe, dataInizio)
+	}
+
+	onCreateSpedizione = () => {
+		const {spedizione, tappe, dataInizio} = this.state
+		return app.inserisciSpedizione(spedizione, tappe, dataInizio)
+	}
+
 	render() {
-		return <InserisciSpedizioneView />
+		return <InserisciSpedizioneView
+			optimization={this.state.optimization}
+			spedizione={this.state.spedizione}
+			camionisti={this.state.camionisti}
+			ordini={this.state.ordini}
+			tappe={this.state.tappe}
+			onInfoVeicoloInserted={this.onInfoVeicoliInserted}
+			onAllInfoInserted={this.onAllInfoInserted}
+			onCreateSpedizione={this.onCreateSpedizione}/>
 	}
 }
--- src/routes/InserisciSpedizione/InserisciSpedizioneView.tsx
+++ src/routes/InserisciSpedizione/InserisciSpedizioneView.tsx
@@ -1,23 +1,50 @@
 import { i18n } from '../../i18n'
 import * as React from 'react'
 import app from '../../app'
+import App from '../../components/App'
+import Drawer from '../../components/Drawer'
+import { SpedizioneRaw, Camionista, Ordine, TappaRaw } from '../../domain'
 
 interface ViewProps {
+	spedizione: SpedizioneRaw,
+	camionisti: Camionista[],
+	optimization: boolean,
+	tappe: TappaRaw[],
+	ordini: Ordine[],
+	onInfoVeicoloInserted() : void,
+	onAllInfoInserted() : boolean
+	onCreateSpedizione() : boolean
 }
 
+interface ViewState {
+	step: 0 | 1,
+}
+
+const StepComponent : React.ReactNode[] = []
+
+StepComponent[0] = <div>Step1</div>
+StepComponent[1] = <div>Step2</div>
+
 export default class InserisciSpedizioneView
-	extends React.Component<ViewProps> {
+	extends React.Component<ViewProps, ViewState> {
 
 	readonly bundle: i18n
 
 	constructor(props: ViewProps) {
 		super(props)
 		this.bundle = app.getBundle()
+		this.state = { step: 0 }
 	}
+
+
 
 	render() {
 		const b = this.bundle.routes.inserisciSpedizione
 
-		return <div>InserisciSpedizione</div>
+		return <App>
+			<Drawer>
+				{ StepComponent[this.state.step] }
+			</Drawer>
+		</App>
 	}
 }
